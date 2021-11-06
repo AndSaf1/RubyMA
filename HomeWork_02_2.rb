ae28b74c863988351e9099b9e00006c8f7096055
AndSaf <sevenn551@gmail.com>
2021-11-06 22:49:57 +0200

    changes HomeWork_02_2

--- HomeWork_02_2.rb
+++ HomeWork_02_2.rb
@@ -1,10 +1,10 @@
 number = 0
   while number <= 9
-  print "Введіть число: "
+  print "Enter the number: "
 number = gets.to_i
 next if number > 9
   result = number * 256
   puts result
 break if number > 9
 end
- puts "Умови недотримані, зупиняю виконання програми :("
+ puts "Conditions are not met, I stop the program :("
