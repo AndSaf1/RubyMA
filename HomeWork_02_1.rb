a6107e83d1041e9b9d4e1988b3d8bc8a9d67d629
AndSaf <sevenn551@gmail.com>
2021-11-10 17:39:13 +0200

    changes HomeWork_02

--- HomeWork_02_1.rb
+++ HomeWork_02_1.rb
@@ -3,17 +3,13 @@ animal = gets.chomp.capitalize
 
 case 
   when animal == 'Horse'
-  puts animal + " talking igo-go. :)"
-
+  puts "#{animal} talking igo-go. :)"
   when animal == 'Cow'
-  puts animal + " talking mu-u-u."
-
+  puts "#{animal} talking mu-u-u. :)"
   when  animal == 'Pig'
-  puts animal + " talking hryu-hryu. :)"
-
+  puts "#{animal} talking hryu-hryu. :)"
   when animal == 'Goat'
-  puts animal + " talking be-e-e. :)"
-
+  puts "#{animal} talking be-e-e. :)"
 else
   puts "There is no such animal in the program. :("
 end
--- HomeWork_02_2.rb
+++ HomeWork_02_2.rb
@@ -1,10 +1,9 @@
-  number = 0
+number=0
 begin
   print "Enter the number: "
   number = gets.chomp.to_i
   num_string = number.to_s
-  num_array = num_string.split('')
-  sum = num_array.select {|v| v =~ /[1]/}.length
+  sum = num_string.split('').count('1')
 break if sum > 1
   result = number * 256
   puts result
