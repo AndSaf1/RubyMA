ba1b77d725b34a8b2088f6066ebb9fe103a0d2a9
AndSaf <sevenn551@gmail.com>
2021-11-22 23:23:04 +0200

    changes HomeWork 03

--- HomeWork_03_1_4.rb
+++ HomeWork_03_1_4.rb
@@ -2,11 +2,11 @@ def errors
     err = []
     err << 'if the value is empty' if firstname.strip.empty?
     err << 'if the value is not a string' if firstname.class != String
-    err << 'value must be at least three characters' if firstname.length > 3
+    err << 'value must be at least three characters' if firstname.length < 3
 
     err << 'if the value is empty' if lastname.strip.empty?
     err << 'if the value is not a string' if lastname.class != String
-    err << 'value must be at least three characters' if lastname.length > 3
+    err << 'value must be at least three characters' if lastname.length < 3
 
     err << 'if the value is empty' if email.strip.empty?
     err << 'if the value is not a string' if email.class != String
